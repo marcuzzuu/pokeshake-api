6b5149aea5d2d907a3d778c32f9b78ce7dd0a15f
marcuzzuu <simone.mancuso@cegeka.it>
2020-06-10 12:28:16 +0200

    Added cache

---
+++ src/main/java/it/marcuzzuu/pokeshakeapi/configuration/CacheConfiguration.java
@@ -0,0 +1,21 @@
+package it.marcuzzuu.pokeshakeapi.configuration;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration("cacheConfiguration")
+public class CacheConfiguration
+{
+	@Bean("operationsCache")
+	@Scope("prototype")
+	public Cache<String, Integer> operationsCache(@Value("${cache.pokemons.capacity:10000}") final Integer maxCapacity, @Value("${cache.pokemons.expire-after-seconds:120}") final Integer expireAfterSeconds)
+	{
+		return Caffeine.newBuilder().maximumSize(maxCapacity).expireAfterWrite(expireAfterSeconds, TimeUnit.SECONDS).build();
+	}
+}
