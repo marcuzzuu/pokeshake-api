6c66cc697df822e28800c6c275fa6e1f5fd77032
mancusom <simone.mancuso@cegeka.it>
2020-06-10 12:16:08 +0200

    Added project structure from initialzr

---
+++ src/main/java/it/marcuzzuu/pokeshakeapi/PokeshakeApiApplication.java
@@ -0,0 +1,13 @@
+package it.marcuzzuu.pokeshakeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeshakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeshakeApiApplication.class, args);
+	}
+
+}
