ff619a6342d6cc4d5695f8838b63a0da50e3a4fa
marcuzzuu <simone.mancuso@cegeka.it>
2020-06-11 19:21:35 +0200

    wip

--- src/test/java/it/marcuzzuu/pokeshakeapi/integration/PokemonControllerTest.java
+++ src/test/java/it/marcuzzuu/pokeshakeapi/integration/PokemonControllerTest.java
@@ -1,53 +1,51 @@
-//package it.marcuzzuu.pokeshakeapi.integration;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import it.marcuzzuu.pokeshakeapi.controller.PokemonController;
-//import it.marcuzzuu.pokeshakeapi.service.impl.PokemonService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//@SpringBootTest
-//@WebMvcTest(PokemonController.class)
-//class PokemonControllerTest
-//{
-//	@Autowired
-//	private MockMvc mvc;
-//	@MockBean
-//	private PokemonService pokemonService;
-//	@Autowired
-//	private ObjectMapper mapper;
-//
-//	@Test
-//	void getDescriptionWithValidNameShouldReturnShakespeareanDescriptionAnd200()
-//	{
-//
-//	}
-//
-//	@Test
-//	void getDescriptionWithInValidNameShouldReturnGivenNameAnd404()
-//	{
-//
-//	}
-//
-//	@Test
-//	void getDescriptionWithNoNameShouldReturn404()
-//	{
-//
-//	}
-//
-//	@Test
-//	void getDescriptionWithValidNameAndErrorsOccurredShouldReturn500()
-//	{
-//
-//	}
-//
-//	@Test
-//	void getDescriptionWithValidNameAndTooManyRequestsOccurredShouldReturn429()
-//	{
-//
-//	}
-//}
+package it.marcuzzuu.pokeshakeapi.integration;
+
+import it.marcuzzuu.pokeshakeapi.controller.PokemonController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.util.Assert;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PokemonControllerTest
+{
+	@Autowired
+	private TestRestTemplate restTemplate;
+	@Autowired
+	private PokemonController controller;
+
+	@Test
+	void getDescriptionWithValidNameShouldReturnShakespeareanDescriptionAnd200()
+	{
+		Assert.notNull(this.controller, "REST Controller is null");
+	}
+
+	@Test
+	void getDescriptionWithInValidNameShouldReturnGivenNameAnd404()
+	{
+
+	}
+
+	@Test
+	void getDescriptionWithNoNameShouldReturn404()
+	{
+
+	}
+
+	@Test
+	void getDescriptionWithValidNameAndErrorsOccurredShouldReturn500()
+	{
+
+	}
+
+	@Test
+	void getDescriptionWithValidNameAndTooManyRequestsOccurredShouldReturn429()
+	{
+
+	}
+}
