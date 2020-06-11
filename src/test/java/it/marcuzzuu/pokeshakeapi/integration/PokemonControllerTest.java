d5440369688c0130880348903aff9d83d740ca2d
marcuzzuu <simone.mancuso@cegeka.it>
2020-06-11 14:28:07 +0200

    wip

--- src/test/java/it/marcuzzuu/pokeshakeapi/integration/PokemonControllerTest.java
+++ src/test/java/it/marcuzzuu/pokeshakeapi/integration/PokemonControllerTest.java
@@ -1,5 +1,53 @@
-package it.marcuzzuu.pokeshakeapi.integration;
-
-public class PokemonControllerTest
-{
-}
+//package it.marcuzzuu.pokeshakeapi.integration;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import it.marcuzzuu.pokeshakeapi.controller.PokemonController;
+//import it.marcuzzuu.pokeshakeapi.service.impl.PokemonService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@SpringBootTest
+//@WebMvcTest(PokemonController.class)
+//class PokemonControllerTest
+//{
+//	@Autowired
+//	private MockMvc mvc;
+//	@MockBean
+//	private PokemonService pokemonService;
+//	@Autowired
+//	private ObjectMapper mapper;
+//
+//	@Test
+//	void getDescriptionWithValidNameShouldReturnShakespeareanDescriptionAnd200()
+//	{
+//
+//	}
+//
+//	@Test
+//	void getDescriptionWithInValidNameShouldReturnGivenNameAnd404()
+//	{
+//
+//	}
+//
+//	@Test
+//	void getDescriptionWithNoNameShouldReturn404()
+//	{
+//
+//	}
+//
+//	@Test
+//	void getDescriptionWithValidNameAndErrorsOccurredShouldReturn500()
+//	{
+//
+//	}
+//
+//	@Test
+//	void getDescriptionWithValidNameAndTooManyRequestsOccurredShouldReturn429()
+//	{
+//
+//	}
+//}
--- src/test/java/it/marcuzzuu/pokeshakeapi/unit/PokemonControllerTest.java
+++ src/test/java/it/marcuzzuu/pokeshakeapi/unit/PokemonControllerTest.java
@@ -1,5 +1,41 @@
 package it.marcuzzuu.pokeshakeapi.unit;
 
-public class PokemonControllerTest
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.marcuzzuu.pokeshakeapi.controller.PokemonController;
+import it.marcuzzuu.pokeshakeapi.service.impl.PokemonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@WebMvcTest(PokemonController.class)
+class PokemonControllerTest
 {
+	@Autowired
+	private MockMvc mvc;
+	@MockBean
+	private PokemonService pokemonService;
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	void getDescriptionWithValidNameShouldReturnShakespeareanDescriptionAnd200()
+	{
+
+	}
+
+	@Test
+	void getDescriptionWithInValidNameShouldReturnGivenNameAnd404()
+	{
+
+	}
+
+	@Test
+	void getDescriptionWithNoNameShouldReturn404()
+	{
+
+	}
 }
